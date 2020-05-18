1d2655e77eea34146df45180038353a52687eb83
mzuzu <30974507+mzuzu@users.noreply.github.com>
2020-05-18 22:24:28 +0800

    Create Start.java

---
+++ Halma_v3/src/xyz/chengzi/halma/Start.java
@@ -0,0 +1,9 @@
+package xyz.chengzi.halma;
+
+import xyz.chengzi.halma.view.StartMenu;
+
+public class Start {
+    public static void main(String[] args) {
+        StartMenu startMenu = new StartMenu();
+    }
+}
