161b10f4a003242b39edb56ce1dea6ae76288d2b
mzuzu <30974507+mzuzu@users.noreply.github.com>
2020-05-23 21:41:31 +0800

    Update GameFrame.java

--- Halma_v3/src/xyz/chengzi/halma/view/GameFrame.java
+++ Halma_v3/src/xyz/chengzi/halma/view/GameFrame.java
@@ -1,11 +1,13 @@
 package xyz.chengzi.halma.view;
 
+import xyz.chengzi.halma.Halma;
+
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
-        setTitle("2020 CS102A Project Demo");
-        setSize(776, 810);
+        setTitle("Halma Game");
+        setSize(776, 820);
         setLocationRelativeTo(null); // Center the window
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(null);
@@ -15,10 +17,35 @@ public class GameFrame extends JFrame {
         statusLabel.setSize(200, 10);
         add(statusLabel);
 
-        JButton button = new JButton("menu");
+        JButton button = new JButton("...");
         button.addActionListener((e) -> JOptionPane.showMessageDialog(this, "Button clicked!"));
         button.setLocation(740, 760);
         button.setSize(20, 12);
         add(button);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLocation(0,0);
+        menuBar.setSize(776,20);
+
+        JMenu menu = new JMenu("Menu");
+
+        JMenuItem save = new JMenuItem("Save");
+        save.addActionListener((e) -> Halma.chessBoard.save());
+
+        JMenuItem load = new JMenuItem("Load");
+        load.addActionListener((e) -> Halma.startLoad());
+
+        JMenuItem replay = new JMenuItem("Replay");
+
+
+        JMenuItem neg = new JMenuItem("New Game");
+
+
+        menu.add(save);
+        menu.add(load);
+        menu.add(replay);
+        menu.add(neg);
+        menuBar.add(menu);
+        add(menuBar);
     }
 }
